f3dceed4db894d57ab67650f587f923f5188ea3b
mai <m.kanda.cingroup2012@gmail.com>
2024-02-12 20:56:00 +0900

    first commit

---
+++ lesson3.php
@@ -0,0 +1,23 @@
+﻿<?php
+// 連想配列のkey
+// name, age, genderに
+// 山田,  20,  女性
+// という値を格納し、
+
+// 山田
+// 20歳・女性
+
+// という形で出力してください。
+
+$lesson3_arr = [
+
+    'name'   => '山田',
+
+    'age'    => '20',
+
+    'gender' => '女性'
+
+];
+
+echo $lesson3_arr['name'] . '<br/>';
+echo $lesson3_arr['age'] . '歳・' . $lesson3_arr['gender'];
